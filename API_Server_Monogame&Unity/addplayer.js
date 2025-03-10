72d9e3564d4f15d30d8f2fca7a76d75def63f918
jayr7799 <129092746+jayr7799@users.noreply.github.com>
2025-03-10 08:31:01 -0400

    Update addplayer.js

--- API_Server_Monogame&Unity/addplayer.js
+++ API_Server_Monogame&Unity/addplayer.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const Player = require("./models/player");
 const {nanoid} = require("nanoid");
 
-mongoose.connect("mongodb+srv://jayrossi204:Trace7799@cluster0.jtkdp.mongodb.net/GamesDB?retryWrites=true&w=majority&appName=Cluster0");
+mongoose.connect("");
 
 async function addPlayer(){
     await Player.create({
